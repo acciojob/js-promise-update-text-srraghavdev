944c3b769e8b97e017a073972fb1957a99d5a19b
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-23 23:54:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let p = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Hello,world!")
+	},1)
+})
+p.then((data)=>{
+	document.getElementById("output").innerText=data
+})
